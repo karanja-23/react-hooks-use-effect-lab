ac5e120bb11a56574ae234a9b3977f8be76b72cd
Hosea <karanja.mungai23@gmail.com>
2024-11-04 11:35:48 +0300

    modifies react components

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, {useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(()=>{
+    
+     const timerID = setInterval(()=>{
+      if (timeRemaining > 0){
+        setTimeRemaining(timeRemaining-1)
+      }
+    
+      else{        
+        setTimeRemaining(10)
+        onAnswered(false)        
+      }      
+        
+      },1000)
+
+      return ()=>{
+        clearInterval(timerID)
+      }
+  
+  },[timeRemaining])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
